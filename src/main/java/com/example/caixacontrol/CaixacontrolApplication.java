ef275169f70208eced41943d794d5c9c8e10b6d0
niculox <nicolasamaral1997@gmail.com>
2025-03-06 20:15:22 -0300

    refactor application properties and Entry model for consistency; update database URL and formatting

--- src/main/java/com/example/caixacontrol/CaixacontrolApplication.java
+++ src/main/java/com/example/caixacontrol/CaixacontrolApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CaixacontrolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CaixacontrolApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CaixacontrolApplication.class, args);
+    }
 }
