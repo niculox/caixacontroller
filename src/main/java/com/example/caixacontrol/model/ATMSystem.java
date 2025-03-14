bedae0206ff811100cb82f32adc96e6b11ba33d9
niculox <nicolasamaral1997@gmail.com>
2025-03-14 13:02:42 -0300

    remove unused ATMSystemRepository and clean up ATMSystem model

--- src/main/java/com/example/caixacontrol/model/ATMSystem.java
+++ src/main/java/com/example/caixacontrol/model/ATMSystem.java
@@ -3,21 +3,14 @@ package com.example.caixacontrol.model;
 import java.util.List;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
-@Table(name = "ATM_SYSTEM")
 public class ATMSystem extends AbstractEntity<Long>{
 
     @Column(nullable = false)
     private List<Entry> entries;
-
-    @Column(nullable = false)
-    private float output;
     
 }
--- src/main/java/com/example/caixacontrol/repository/IATMSystemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.caixacontrol.repository;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.caixacontrol.model.ATMSystem;
-
-public interface IATMSystemRepository extends JpaRepository<ATMSystem, Long>{
-    List<ATMSystem> findByOutput(float output);
-}
